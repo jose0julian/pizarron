7794e1649e367d45ccc654f219079953f5f29006
jose0julian <81386110+jose0julian@users.noreply.github.com>
2022-07-11 13:55:49 -0500

    Create index.py
    some parts of python

---
+++ index.py
@@ -0,0 +1,13 @@
+# The use boolena(Decision with true or false)
+
+if(True):
+  print("The condition is True")
+  
+else:
+  print("The condition is False")
+  
+  
+# The use of Condition ( for ) with count of 1 to 10
+for i in range(1, 10):
+  print(i)
+
